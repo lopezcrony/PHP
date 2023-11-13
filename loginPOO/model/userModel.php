4108d4951d673aadbc25f96bab2f94021558d50c
lopezcrony <lopezcrony@gmail.com>
2023-11-13 16:12:30 -0500

    add alert

--- loginPOO/model/userModel.php
+++ loginPOO/model/userModel.php
@@ -1,34 +1,34 @@
 <?php
 
+use PDO;
+
 class userModel{
 
     static public function mdlUser($table, $data){
 
-        $search=conexBD::FunctionConex()->prepare("SELECT * FROM usuario WHERE email = :email");
-        $search->bindParam("email:", $data['email'], PDO::PARAM_STR);
-        $search->execute();
+        $statm = conexBD::FunctionConex()->prepare("SELECT * FROM usuario WHERE email = :email");
+        $statm->bindParam(":email", $data['email'], PDO::PARAM_STR);
+        $statm->execute();
 
-        $user= $search->fetch(PDO::FETCH_ASSOC);
+        $user = $statm->fetch(PDO::FETCH_ASSOC);
 
-        if ($user == false) {
+        if ($user === false) {
             echo "usuario no encontrado";
         }
-            $passwordValidate=password_verify($data['password'], $user['password']);
-            if ($passwordValidate) {
-                session_start();
+        $passwordValidate = password_verify($data['password'], $user['password']);
+        if ($passwordValidate) {
+            session_start();
 
-                $_SESSION['fullname']=$user['nombreUsuario']." ".$_SESSION['apellidoUsuario'];
-                $_SESSION['email']=$user['email'];
-                
-                return "ok";
-            }else{
-                echo "credenciales incorrectas";
-            }
-    
-            return $user;
-            $search=null;
+            $_SESSION['fullname'] = $user['nombreUsuario'] . " " . $_SESSION['apellidoUsuario'];
+            $_SESSION['email'] = $user['email'];
+
+            return "ok";
+        } else {
+            echo "credenciales incorrectas";
+        }
+
+        return $user;
+        $statm = null;
     }
-
-   
-
 }
+
